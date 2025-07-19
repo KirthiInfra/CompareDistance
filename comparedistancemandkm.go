010918ea18ec08f822a99470531e0805b1cbce98
KirthiInfra <kirthivasan@infraspec.dev>
2025-07-19 21:57:38 +0530

    Add function to check the two distances of various values and units and its tests
    Co-authored by KV
    Co-authored by Sujeet

--- comparedistancemandkm.go
+++ comparedistancemandkm.go
@@ -32,8 +32,19 @@ func main() {
 }
 
 func UnitSupportedOrNot(d1 *distance, d2 distance) error {
-	if (d1.unit != "m" && d1.unit != "km") || (d2.unit != "m" && d2.unit != "km") {
+	if (d1.unit != m && d1.unit != km) || (d2.unit != m && d2.unit != km) {
 		return errors.New("invalid unit, supported units (m or km)")
 	}
 	return nil
+}
+
+func (d1 *distance) CompareDistances(d2 distance) bool {
+	if d1.unit == d2.unit {
+		return d1.value == d2.value
+	} else {
+		if (d1.unit == km && d1.value*1000 == d2.value) || (d2.unit == km && d2.value*1000 == d1.value) {
+			return true
+		}
+		return false
+	}
 }
--- comparedistancemandkm_test.go
+++ comparedistancemandkm_test.go
@@ -16,3 +16,46 @@ func TestUnitSupportedOrNot(t *testing.T) {
 		t.Errorf("%v", err)
 	}
 }
+
+func TestCompareDistance(t *testing.T) {
+	tests := []struct {
+		name     string
+		d1       distance
+		d2       distance
+		expected bool
+	}{
+		{
+			name:     "1000 meters equal to 1000 meters",
+			d1:       distance{value: 1000, unit: m},
+			d2:       distance{value: 1000, unit: m},
+			expected: true,
+		},
+		{
+			name:     "1 meter does not equal to 2 meters",
+			d1:       distance{value: 1, unit: m},
+			d2:       distance{value: 2, unit: m},
+			expected: false,
+		},
+		{
+			name:     "1000 meters equal to 1 kilometer",
+			d1:       distance{value: 1000, unit: m},
+			d2:       distance{value: 1, unit: km},
+			expected: true,
+		},
+		{
+			name:     "1 kilometer equals to 1000 meters",
+			d1:       distance{value: 1, unit: km},
+			d2:       distance{value: 1000, unit: m},
+			expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.d1.CompareDistances(tt.d2)
+			if got != tt.expected {
+				t.Errorf("CompareDistances() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
